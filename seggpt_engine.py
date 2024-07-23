97ec45f99aa544ebcdefe5c43021e4a8edd9cab9
John Walsh <johnwalsh7412@gmail.com>
2024-07-23 16:05:40 -0400

    add patching functionality

--- seggpt_engine.py
+++ seggpt_engine.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 import random
-import cv2
 from tqdm import tqdm
 
 from PIL import Image
@@ -14,6 +13,9 @@ imagenet_std = np.array([0.229, 0.224, 0.225])
 
 
 class Cache(list):
+    """
+    A class for a FIFO cache with a specified maximum size.
+    """
     def __init__(self, max_size=0):
         super().__init__()
         self.max_size = max_size
@@ -28,6 +30,9 @@ class Cache(list):
 
 @torch.no_grad()
 def run_one_image(img, tgt, model, device):
+    """
+    Run inference with the given input image and target image. 
+    """
     x = torch.tensor(img)
     # make it a batch-like
     x = torch.einsum('nhwc->nchw', x)
@@ -64,8 +69,8 @@ def to_patches(image, patch_size):
     img_width, img_height = image.size
     
     # Calculate the new dimensions
-    new_width = (img_width // patch_size) * patch_size
-    new_height = (img_height // patch_size) * patch_size
+    new_width = max((img_width // patch_size), 1) * patch_size
+    new_height = max((img_height // patch_size), 1) * patch_size
 
     # Resize the image
     resized_image = image.resize((new_width, new_height))
@@ -75,7 +80,6 @@ def to_patches(image, patch_size):
     num_patches_x = resized_image.width // patch_size
     num_patches_y = resized_image.height // patch_size
 
-    patch_id = 0
     for i in range(num_patches_x):
         for j in range(num_patches_y):
             # Calculate the bounding box of the current patch
@@ -89,7 +93,12 @@ def to_patches(image, patch_size):
     return patches
 
 
-def inference_image_dir(model, device, input_dir, prompt_dir, target_dir, out_dir, patchify):
+def inference_image_dir(model, device, input_dir, prompt_dir, target_dir, out_dir, patchify, num_patches):
+    """
+    Run inference on the images in the given input directory, using prompts and targets from the given prompt and
+    target directories, respectively. If patchify is specified, divides the prompt and target images into patches, 
+    and randomly selects the specified number of patches to use as model context.
+    """
     print('patchify: ', patchify)
     prompt_names = os.listdir(prompt_dir)
     prompt_paths = [os.path.join(prompt_dir, prompt_img_name) for prompt_img_name in prompt_names]
@@ -111,12 +120,8 @@ def inference_image_dir(model, device, input_dir, prompt_dir, target_dir, out_di
            target_images.extend(to_patches(target_image, patch_size))
 
         paired_list = list(zip(prompt_images, target_images))
-        samples = random.sample(paired_list, 8)
+        samples = random.sample(paired_list, num_patches)
         prompt_images, target_images = zip(*samples)
-        for np, p in enumerate(prompt_images):
-            p.save(f'prompt_{np}.png')
-        for nt, t in enumerate(target_images):
-            t.save(f'target_{nt}.png')
     else:
         prompt_images = [Image.open(img).convert("RGB") for img in prompt_paths]
         target_images = [Image.open(img).convert("RGB") for img in target_paths]
@@ -128,6 +133,10 @@ def inference_image_dir(model, device, input_dir, prompt_dir, target_dir, out_di
 
 
 def inference_and_save_image(model, device, img_path, prompt_images, target_images, out_path, patchify):
+    """
+    Run inference on the image at the given img_path, using the given prompt and target images. If patchify is 
+    specified, divides the images into separate patches to reduce data loss from resizing.
+    """
     patch_size = 448
     input_image = Image.open(img_path).convert("RGB")
     if patchify:
@@ -135,12 +144,8 @@ def inference_and_save_image(model, device, img_path, prompt_images, target_imag
         annotated_patches = []
         num_patches_x = input_image.width // patch_size
         num_patches_y = input_image.height // patch_size
-        pn = 0
-        for patch in tqdm(patches):
-            patch.save(f'patch_{pn}.png')
+        for patch in tqdm(patches, position=1, leave=False):
             annotated_patches.append(inference_image(model, device, patch, prompt_images, target_images))
-            Image.fromarray(annotated_patches[-1].astype(np.uint8)).save(f'test{len(annotated_patches)}.png')
-            pn += 1
 
         reconstructed_image = Image.new('RGB', (num_patches_x * patch_size, num_patches_y * patch_size))
         patch_id = 0
@@ -162,6 +167,9 @@ def inference_and_save_image(model, device, img_path, prompt_images, target_imag
 
 
 def inference_image(model, device, input_image, prompt_images, target_images):
+    """
+    Run inference on the given input image, using the given prompt images and target images.
+    """
     res, hres = 448, 448
 
     # Open the input image and resize it
@@ -211,108 +219,3 @@ def inference_image(model, device, input_image, prompt_images, target_images):
     ).permute(0, 2, 3, 1)[0].numpy()
 
     return output
-
-
-def inference_video(model, device, vid_path, num_frames, img2_paths, tgt2_paths, out_path, img_path, anno_color=''):
-    # currently only checks for mp4 file
-    using_video = vid_path[-3:] == 'mp4'
-    
-    res, hres = 448, 448
-
-    if using_video:
-        cap = cv2.VideoCapture(vid_path)
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        video_writer = cv2.VideoWriter(out_path, fourcc, fps, (width, height), True)
-    else:
-        frames = [os.path.join(vid_path, file) for file in sorted(os.listdir(vid_path))]
-        ff = 0
-    
-    if img2_paths is None:
-        if using_video:
-            _, frame = cap.read()
-        else:
-            frame = cv2.imread(frames[ff])
-            ff += 1
-
-        img2 = [np.array(Image.fromarray(frame[:, :, ::-1]).convert('RGB').resize((res, hres))) / 255.]
-    else:
-        img2 = [np.array(Image.open(img2_path).convert("RGB").resize((res, hres))) / 255. for img2_path in img2_paths]
-
-    tgt2 = [np.array(Image.open(tgt2_path).convert("RGB").resize((res, hres), Image.NEAREST)) / 255. for tgt2_path in tgt2_paths]
-
-    frames_cache, target_cache = Cache(num_frames), Cache(num_frames)
-
-    i = 0
-    while True:
-        if using_video:
-            ret, frame = cap.read()
-        else:
-            ret = ff < len(frames)
-            if ret:
-                frame_name = frames[ff]
-                frame = cv2.imread(frame_name)
-                ff += 1
-
-        if not ret:
-            break
-
-        image_batch, target_batch = [], []
-        image = Image.fromarray(frame[:, :, ::-1]).convert('RGB')
-        input_image = np.array(image)
-        size = image.size
-        image = np.array(image.resize((res, hres))) / 255.
-
-        for prompt, target in zip(img2 + frames_cache, tgt2 + target_cache):
-            tgt = target  # tgt is not available
-            tgt = np.concatenate((target, tgt), axis=0)
-            img = np.concatenate((prompt, image), axis=0)
-
-            assert img.shape == (2*res, res, 3), f'{img.shape}'
-            # normalize by ImageNet mean and std
-            img = img - imagenet_mean
-            img = img / imagenet_std
-
-            assert tgt.shape == (2*res, res, 3), f'{img.shape}'
-            # normalize by ImageNet mean and std
-            tgt = tgt - imagenet_mean
-            tgt = tgt / imagenet_std
-
-            image_batch.append(img)
-            target_batch.append(tgt)
-
-        img = np.stack(image_batch, axis=0)
-        tgt = np.stack(target_batch, axis=0)
-
-        torch.manual_seed(2)
-        output = run_one_image(img, tgt, model, device)
-
-        frames_cache.append(image)
-        target_cache.append(
-            output.mean(-1) \
-                .gt(128).float() \
-                .unsqueeze(-1).expand(-1, -1, 3) \
-                .numpy()
-        )
-
-        output = F.interpolate(
-            output[None, ...].permute(0, 3, 1, 2), 
-            size=[size[1], size[0]], 
-            mode='nearest',
-        ).permute(0, 2, 3, 1)[0].numpy()
-        if using_video:
-            img_name = img_path % i
-        else:
-            img_name = os.path.join(img_path, os.path.splitext(os.path.basename(frame_name))[0] + '.png')
-        img_to_write = np.tile((output.mean(-1) > 128).astype(np.float32)[:, :, np.newaxis], (1, 1, 3)).astype(np.uint8)
-        img_to_write[:] = anno_color.split()
-        cv2.imwrite(img_name, img_to_write)
-        if using_video:
-            output = input_image * (0.6 * output / 255 + 0.4)
-            video_writer.write(np.ascontiguousarray(output.astype(np.uint8)[:, :, ::-1]))
-        i += 1
-
-    if using_video:
-        video_writer.release()
--- seggpt_inference.py
+++ seggpt_inference.py
@@ -1,41 +1,37 @@
-import os
 import torch
 import argparse
 
-from seggpt_engine import inference_image, inference_image_dir, inference_video
+from seggpt_engine import inference_image_dir
 import seggpt_models
 
 
 def get_args_parser():
+    """
+    Parse arguments for running SegGPT inference.
+    """
     parser = argparse.ArgumentParser('SegGPT inference', add_help=False)
     parser.add_argument('--ckpt_path', type=str, help='path to ckpt',
                         default='seggpt_vit_large.pth')
     parser.add_argument('--model', type=str, help='specific model name',
                         default='seggpt_vit_large_patch16_input896x448')
-    parser.add_argument('--input_image', type=str, help='path to input image to be tested',
-                        default=None)
-    parser.add_argument('--input_video', type=str, help='path to input video to be tested',
-                        default=None)
-    parser.add_argument('--num_frames', type=int, help='number of prompt frames in video',
-                        default=0)
-    parser.add_argument('--prompt_image', type=str, nargs='+', help='path to prompt image',
-                        default=None)
-    parser.add_argument('--prompt_target', type=str, nargs='+', help='path to prompt target',
-                        default=None)
     parser.add_argument('--seg_type', type=str, help='embedding for segmentation types', 
                         choices=['instance', 'semantic'], default='instance')
     parser.add_argument('--device', type=str, help='cpu, cuda, or mps (for silicon macs)',
                         default='cpu')
     parser.add_argument('--output_dir', type=str, help='path to output',
                         default='./')
-    parser.add_argument('--input_dir', type=str, help='directory of input images to be segmented')
-    parser.add_argument('--prompt_dir', type=str, help='directory of prompt images to use as context')
-    parser.add_argument('--target_dir', type=str, help='directory of target images, i.e., binary mask images of the prompt images')
+    parser.add_argument('--input_dir', type=str, required=True, help='directory of input images to be segmented')
+    parser.add_argument('--prompt_dir', type=str, required=True, help='directory of prompt images to use as context')
+    parser.add_argument('--target_dir', type=str, required=True, help='directory of target images, i.e., binary mask images of the prompt images')
     parser.add_argument('--patch_images', action='store_true', help='divide images into 448x448 patches')
+    parser.add_argument('--num_prompts', type=int, default=8, help='The number of prompt/targets to use if patching images')
     return parser.parse_args()
 
 
 def prepare_model(chkpt_dir, arch='seggpt_vit_large_patch16_input896x448', seg_type='instance'):
+    """
+    Load the specified configuration type and checkpoint.
+    """
     # build model
     model = getattr(seggpt_models, arch)()
     model.seg_type = seg_type
@@ -53,28 +49,5 @@ if __name__ == '__main__':
     model = prepare_model(args.ckpt_path, args.model, args.seg_type).to(device)
     print('Model loaded.')
 
-    # Directory of input images, directory of prompt images, directory of target images
-    if args.input_dir is not None:
-        assert args.prompt_dir is not None and args.target_dir is not None
-
-        inference_image_dir(model, device, args.input_dir, args.prompt_dir, args.target_dir, args.output_dir, args.patch_images)
-
-    # Input image, prompt image, target image
-    if args.input_image is not None:
-        assert args.prompt_image is not None and args.prompt_target is not None
-
-        img_name = os.path.basename(args.input_image)
-        out_path = os.path.join(args.output_dir, "output_" + '.'.join(img_name.split('.')[:-1]) + '.png')
-
-        inference_image(model, device, args.input_image, args.prompt_image, args.prompt_target, out_path)
-    
-    # Input image, prompt image, target image
-    if args.input_video is not None:
-        assert args.prompt_target is not None
-        vid_name = os.path.basename(args.input_video)
-        out_path = os.path.join(args.output_dir, "output_" + '.'.join(vid_name.split('.')[:-1]) + '.mp4')
-        img_path = os.path.join(args.output_dir, "output_" + '.'.join(vid_name.split('.')[:-1]) + '_%d.png')
-
-        inference_video(model, device, args.input_video, args.num_frames, args.prompt_image, args.prompt_target, out_path, img_path)
-
-    print('Finished.')
+    # Perform inference on a directory of input images, directory of prompt images, and directory of target images
+    inference_image_dir(model, device, args.input_dir, args.prompt_dir, args.target_dir, args.output_dir, args.patch_images, args.num_prompts)
